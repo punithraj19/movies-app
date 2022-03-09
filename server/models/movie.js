2f7f3e984dd8faa7d8329abb5e4275e62a61497a
punithraj19 <punithraj.t@byjus.com>
2022-03-09 13:16:55 +0530

    Updated code in movies app

---
+++ server/models/movie.js
@@ -0,0 +1,29 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const Movie = sequelize.define('Movie', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        poster: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {
+        freezeTableName: true,
+        timestamps: true,
+        schema: 'public',
+    });
+
+    return Movie;
+}
